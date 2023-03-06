21a568806e435403698f87cf329d283e713a0728
Kush <kushpatel@gmail.com>
2023-03-05 18:35:42 -0800

    viz

--- src/visualize.py
+++ src/visualize.py
@@ -14,6 +14,7 @@ import json
 from collections import Counter,defaultdict
 import matplotlib
 matplotlib.use('Agg')
+matplotlib.rc('font', family='Arial')
 import matplotlib.pyplot as plt
 import heapq
 import pandas as pd
